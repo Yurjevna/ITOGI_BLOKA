2095231734a93e93e94a31c1c44d9b4d5a08e591
An <an@mail.ru>
2022-09-20 20:37:36 +0300

    задача

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string text = "12354, hello, war, no";
+string p = text.Replace(" ", "");
+string[] words = p.Split(',');
+for (int i = 0; i < words.Length; i++)
+{
+    Console.WriteLine(words[i] + "\t" + words[i].Length);
+}
+Console.WriteLine();
+
+string[] words1 = new string[words.Length];
+
+void FillArr(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array1[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArr(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+}
+
+FillArr(words, words1);
+PrintArr(words1);
